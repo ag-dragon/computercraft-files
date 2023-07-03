7b75c0ac5cd4905f4a7f94e7ca80bfef37837b1d
ag-dragon <94580774+ag-dragon@users.noreply.github.com>
2023-07-03 14:44:25 -0400

    Add Filelist Updating

---
+++ filelist.lua
@@ -0,0 +1,13 @@
+local includes = {
+        -- includes
+        "include/shorthand.hex",
+        "include/raycast.hex",
+        }
+
+local spells = {
+        -- spells
+        "spells/railgun.hex",
+        "spells/smartbomb.hex",
+        }
+
+return includes, spells
--- update.lua
+++ update.lua
@@ -6,6 +6,8 @@ shell.run("delete", "hex_compiler.lua")
 os.sleep(.4)
 shell.run("delete", "patterns.lua")
 os.sleep(.4)
+shell.run("delete", "filelist.lua")
+os.sleep(.4)
 
 shell.run("wget", "https://raw.githubusercontent.com/ag-dragon/computercraft-files/master/masterlist.hex", "masterlist.hex")
 os.sleep(.4)
@@ -13,7 +15,24 @@ shell.run("wget", "https://raw.githubusercontent.com/ag-dragon/computercraft-fil
 os.sleep(.4)
 shell.run("wget", "https://raw.githubusercontent.com/ag-dragon/computercraft-files/master/patterns.lua", "patterns.lua")
 os.sleep(.4)
+shell.run("wget", "https://raw.githubusercontent.com/ag-dragon/computercraft-files/master/patterns.lua", "filelist.lua")
+os.sleep(.4)
 
+local includes, spells = require("filelist")
+
+for i,filename in ipairs(includes) do
+    shell.run("delete", "include/" .. filename)
+    os.sleep(.4)
+    shell.run("wget", "https://raw.githubusercontent.com/ag-dragon/computercraft-files/master/include/" .. filename, filename)
+    os.sleep(.4)
+end
+
+for i,filename in ipairs(spells) do
+    shell.run("delete", "include/" .. filename)
+    os.sleep(.4)
+    shell.run("wget", "https://raw.githubusercontent.com/ag-dragon/computercraft-files/master/include/" .. filename, filename)
+    os.sleep(.4)
+end
 
 -- shell.run("delete", "spells")
 -- os.sleep(.4)
