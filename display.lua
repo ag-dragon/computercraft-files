705397f85e336b9c8ee0596a4acf42b6d478a163
ag-dragon <94580774+ag-dragon@users.noreply.github.com>
2023-07-03 15:20:13 -0400

    Change display text

--- display.lua
+++ display.lua
@@ -1,26 +1,20 @@
 -- draw
 -- draws display to monitor
 -- @arg monitor the monitor to draw on
-local function draw(monitor, output)
+local function draw(monitor, list)
     local m_width, m_height = monitor.getSize()
     monitor.setBackgroundColor(colors.black)
     monitor.clear()
     
     local start = 0
     local i = 1
-    if #output > m_height then
-        start = #output - m_height
+    if #list > m_height then
+        start = #list - m_height
         i = start
     end
     
-    while i <= #output do
-        if i == #output then
-            monitor.setBackgroundColor(colors.purple)
-        else
-            monitor.setBackgroundColor(colors.black)
-        end
-            
-        local line = i .. " " .. output[i]
+    while i <= #list do
+        local line = string.sub(list[i], 1, -5)
         monitor.setCursorPos(1, i-start)
         local format = "%." .. m_width .. "s"
         monitor.write(string.format(format, line))
